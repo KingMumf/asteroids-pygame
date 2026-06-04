eb1c33a558db1e7fe55ebfdf690f3b801cf320d4
kingmumf_git <alexrgmumford@gmail.com>
2026-06-04 12:38:25 +0100

    2: Added in FPS config

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
-    screen.fill(BLACK)
+        screen.fill("black")
 
-    pygame.display.flip()
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
